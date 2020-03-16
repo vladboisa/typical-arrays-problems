98a6bf72bd3428fccba7063a3e591c208f2c1e0d
VladBoisa <60569670+vladboisa@users.noreply.github.com>
2020-03-16 16:52:12 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
+exports.min = function min(array) {
+    if (array == undefined || array.length == 0) return 0;
+    const min = Math.min.apply(null, array);
+    return min;
 
-exports.min = function min (array) {
-  return 0;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    if (array == undefined || array.length == 0) return 0;
+    const max = Math.max.apply(null, array);
+    return max;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    if (array == undefined || array.length == 0) return 0;
+    let sum = 0,
+        i = 0,
+        avg = 0;
+    for (i = 0; i < array.length; i++) {
+        sum += array[i];
+    }
+    avg = sum / array.length;
+    return avg;
 }
